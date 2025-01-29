a3b5a0a5f58c8ff63bf540be0352fe01ba3dd74c
J-I-H-O <jeongjiho0731@gmail.com>
2025-01-29 16:40:25 +0900

    feat: BOJ2751

---
+++ JAVA/src/sort/Boj2751.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Boj2751 {
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i] + System.lineSeparator());
+        }
+        bw.flush();
+    }
+}
