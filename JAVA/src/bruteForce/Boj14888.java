0f5789dab7b860c3a919f46257769c865ad90a7a
J-I-H-O <jeongjiho0731@gmail.com>
2025-02-19 12:01:45 +0900

    refactor: BOJ14888 improved

--- JAVA/src/bruteForce/Boj14888.java
+++ JAVA/src/bruteForce/Boj14888.java
@@ -3,51 +3,38 @@ package bruteForce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Boj14888 {
 
     static int N;
     static int[] operands;
-    static List<Character> operators = new ArrayList<>();
-    static List<Character> selected = new ArrayList<>();
-    static boolean[] visited;
+    static int[] operators = new int[4];
     static int min = Integer.MAX_VALUE;
     static int max = Integer.MIN_VALUE;
 
-    static int calc() {
-        int result = operands[0];
-        for (int i = 0; i < selected.size(); i++) {
-            char operator = selected.get(i);
-            if (operator == '+') {
-                result += operands[i + 1];
-            } else if (operator == '-') {
-                result -= operands[i + 1];
-            } else if (operator == '*') {
-                result *= operands[i + 1];
-            } else {
-                result /= operands[i + 1];
-            }
-        }
-        return result;
-    }
-
-    static void permutation() {
-        if (selected.size() == N - 1) {
-            int result = calc();
-            min = Math.min(min, result);
-            max = Math.max(max, result);
+    static void permutation(int curResult, int operandIdx) {
+        // 마지막 피연산자까지 계산 완료된 경우
+        if (operandIdx == N) {
+            min = Math.min(min, curResult);
+            max = Math.max(max, curResult);
             return;
         }
 
-        for (int i = 0; i < operators.size(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                selected.add(operators.get(i));
-                permutation();
-                selected.remove(selected.size() - 1);
-                visited[i] = false;
+        for (int i = 0; i < 4; i++) {
+            if (operators[i] > 0) {
+                operators[i]--;
+
+                if (i == 0) {
+                    permutation(curResult + operands[operandIdx], operandIdx + 1);
+                } else if (i == 1) {
+                    permutation(curResult - operands[operandIdx], operandIdx + 1);
+                } else if (i == 2) {
+                    permutation(curResult * operands[operandIdx], operandIdx + 1);
+                } else {
+                    permutation(curResult / operands[operandIdx], operandIdx + 1);
+                }
+
+                operators[i]++;
             }
         }
     }
@@ -56,7 +43,6 @@ public class Boj14888 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         operands = new int[N];
-        visited = new boolean[N - 1];
 
         String[] input = br.readLine().split(" ");
         for (int i = 0; i < input.length; i++) {
@@ -64,24 +50,11 @@ public class Boj14888 {
         }
 
         input = br.readLine().split(" ");
-        for (int i = 0; i < input.length; i++) {
-            char targetOperator;
-            if (i == 0) {
-                targetOperator = '+';
-            } else if (i == 1) {
-                targetOperator = '-';
-            } else if (i == 2) {
-                targetOperator = '*';
-            } else {
-                targetOperator = '/';
-            }
-
-            for (int j = 0; j < Integer.parseInt(input[i]); j++) {
-                operators.add(targetOperator);
-            }
+        for (int i = 0; i < 4; i++) {
+            operators[i] += Integer.parseInt(input[i]);
         }
 
-        permutation();
+        permutation(operands[0], 1);
         System.out.println(max);
         System.out.println(min);
     }
