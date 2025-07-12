7f98531e23253b1cd9b97e2a2afafc6eaf2cda51
J-I-H-O <jeongjiho0731@gmail.com>
2025-07-12 21:36:14 +0900

    refactor: Boj1946 lambda

--- JAVA/src/greedy/Boj1946.java
+++ JAVA/src/greedy/Boj1946.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Boj1946 {
 
@@ -23,12 +22,7 @@ public class Boj1946 {
             // 서류 점수로 지원자를 오름차순 정렬
             // => index가 높은 지원자는 앞선 지원자보다 서류 점수 등수가 낮은 지원자임.
             // => 따라서 면접 점수 등수가 앞선 모든 지원자보다 높은 경우에만 채용
-            Arrays.sort(score, new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return o1[0] - o2[0];
-                }
-            });
+            Arrays.sort(score, (o1, o2) -> o1[0] - o2[0]);
 
             int ans = 1;
             int topInterview = score[0][1];
